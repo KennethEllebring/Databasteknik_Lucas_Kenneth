51e5c3cdc40c89a05082d7015ba03841d8c87b9e
Lucas Larsson <lucas.larsson93@hotmail.com>
2023-01-19 10:57:25 +0100

    We installed scss to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {Routes, Route, BrowserRouter} from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Home from "./components/home";
 import Register from "./components/register";
 import NotFound from "./components/notfound";
